534e09593ce03e9f17e06ce403566563f0031641
mario.arista <mario.arista.dev@gmail.com>
2024-02-07 15:54:37 +0100

    inserting numbers in grid with function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@ const gridElement = document.querySelector(".row");
 // dichiaro button
 const ButtonElement = document.querySelector("#start-button");
 
+// Funzione per riempire array
+function getNumbersArray(number) {
+
+    // Dichiaro array vuoto
+    const arrayWhitNumbers = [];
+
+    // Faccio un for per 100 numeri per il momento
+    for (let i = 1; i <= 100; i++) {
+
+        // inserisci numeri in array vuoto
+        arrayWhitNumbers.push(i);
+ 
+    }
+
+    // restituisco array pieno
+    return arrayWhitNumbers;
+    
+
+}
+
+
 // Inizio evento al click 
 
 ButtonElement.addEventListener("click", function(event){
@@ -19,11 +40,11 @@ ButtonElement.addEventListener("click", function(event){
         // aggiungo la classe square
         newElement.classList.add("square");
 
+        // aggiungo la classe square
+        newElement.innerText = getNumbersArray()[i];
+
         //aggiungo il nuovo elemento alla row
         gridElement.append(newElement);
     }
-
-
-
 
 });
